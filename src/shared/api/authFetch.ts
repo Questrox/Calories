1c3726634ed5e6065b302cf599748b1ab605bd35
Questrox <frantsuzov.ilya@gmail.com>
2025-11-23 00:13:50 +0300

    NSwag

---
+++ src/shared/api/authFetch.ts
@@ -0,0 +1,15 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+export async function authFetch(url: RequestInfo, init: RequestInit = {}) {
+    const token = await AsyncStorage.getItem("jwt");
+
+    const headers = {
+        ...(init.headers ?? {}),
+        "Authorization": token ? `Bearer ${token}` : undefined,
+    };
+
+    return fetch(url, {
+        ...init,
+        headers
+    });
+}
